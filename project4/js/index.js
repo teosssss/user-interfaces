0636fa65713ad7f4e82a3db309e5c83f5e973888
JakubSzymanowicz <jakub.szymanowicz@gmail.com>
2022-11-02 18:02:55 +0100

    asd

--- project4/js/index.js
+++ project4/js/index.js
@@ -36,3 +36,15 @@ function displayPlaylist(){
         sidebarList.append(el)
     });
 }
+
+var releaseDate = new Date("Dec 24, 2022 18:00").getTime();
+var countdownfunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = releaseDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  }, 1000);
